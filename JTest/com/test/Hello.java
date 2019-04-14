a1a44188d658be197b191c982f792e0fda8b08f1
acczh <1708983040@qq.com>
2019-04-14 10:46:43 +0800

    提交JTest

---
+++ JTest/com/test/Hello.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class Hello {
+	public static void main(String[] args) {
+		Meth m = new Meth();
+		m.test("say hi");
+	}
+}
---
+++ JTest/com/test/Meth.java
@@ -0,0 +1,7 @@
+package com.test;
+
+public class Meth {
+	public void test(String str) {
+		System.out.println(str);
+	}
+}
